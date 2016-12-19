b0bc6bedc628aaa8d5d2b4e89273f4e5c9e3b158
cryzahell <805584986@qq.com>
2016-12-19 18:26:36 +0800

    home ui progress

--- app/src/main/java/com/ox/chengystudio/AcHome.java
+++ app/src/main/java/com/ox/chengystudio/AcHome.java
@@ -3,7 +3,7 @@ package com.ox.chengystudio;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ActHome extends AppCompatActivity {
+public class AcHome extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
