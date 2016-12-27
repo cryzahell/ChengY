6c2381e5b150d8c6ea9f5bfd14fb80cb525cbe63
cryzahell <805584986@qq.com>
2016-12-28 02:08:13 +0800

    wenshi half

--- greendao/src/main/java/com/example/MyGreenDao.java
+++ greendao/src/main/java/com/example/MyGreenDao.java
@@ -9,6 +9,8 @@ public class MyGreenDao {
     public static void main(String[] args) {
         try {
             Schema schema = new Schema(1, "com.ox.greendao");
+            //城市
+            addCity(schema);
             //画
             addEntityHua(schema);
             //时间区间
@@ -19,8 +21,16 @@ public class MyGreenDao {
         }
     }
 
+    private static void addCity(Schema schema) {
+        Entity city = schema.addEntity("City");
+        city.addIdProperty();
+        city.addStringProperty("name").notNull();
+    }
+
     private static void addEntityTimeArea(Schema schema) {
         Entity timeArea = schema.addEntity("TimeArea");
+        timeArea.addIdProperty();
+        timeArea.addLongProperty("huaId");
         timeArea.addLongProperty("startTime").notNull();
         timeArea.addLongProperty("endTime").notNull();
     }
@@ -29,6 +39,7 @@ public class MyGreenDao {
         Entity hua = schema.addEntity("Hua");
         hua.addIdProperty();
         hua.addStringProperty("name").notNull();
+        hua.addLongProperty("cityId");
         hua.addBooleanProperty("hasDrawn").notNull();
     }
 
